8c262c2453f58be85cbfc398463f197f3f61a936
baris <baris.aydinoz@gmail.com>
2017-10-25 16:05:14 +0300

    initial mvn test for simple client

--- oss-pipeline.groovy
+++ oss-pipeline.groovy
@@ -1,67 +1,78 @@
 pipeline {
-  agent {
+    agent {
         label "docker"
     }
 
-  parameters {
-    string(name: 'HOST', defaultValue: 'localhost', description: 'Should be changed when agent have docker')
-    string(name: 'NAME', defaultValue: 'hz-oss-docker', description: 'Image name')
-    string(name: 'SLEEP', defaultValue: '10', description: 'Wait time for Hazelcast STARTED')
-  }
+    parameters {
+        string(name: 'HOST', defaultValue: 'localhost', description: 'Should be changed when agent have docker')
+        string(name: 'NAME', defaultValue: 'hz-oss-docker', description: 'Image name')
+        string(name: 'SLEEP', defaultValue: '10', description: 'Wait time for Hazelcast STARTED')
+    }
 
-  options {
-    timestamps()
-  }
+    options {
+        timestamps()
+    }
 
-  stages {
+    stages {
 
-    stage('Build') {
-      steps {
-        git changelog: false, poll: false, url: 'https://github.com/hazelcast/hazelcast-docker.git'
-        dir('hazelcast-oss') {
-          script{
-            oss = docker.build("${params.NAME}:${env.BUILD_ID}")
-          }
+        stage('Build') {
+            steps {
+                git changelog: false, poll: false, url: 'https://github.com/hazelcast/hazelcast-docker.git'
+                dir('hazelcast-oss') {
+                    script {
+                        oss = docker.build("${params.NAME}:${env.BUILD_ID}")
+                    }
+                }
+            }
         }
-      }
-    }
 
-    stage('Run') {
-      steps {
-        script{
-          oss.withRun("-p 5701:5701"){ container ->
-            sleep params.SLEEP as Integer
-            sh "docker logs ${container.id} --tail 1 2>&1 | grep STARTED"
-          }
+        stage('Run') {
+            steps {
+                script {
+                    oss.withRun("-p 5701:5701") { container ->
+                        sleep params.SLEEP as Integer
+                        sh "docker logs ${container.id} --tail 1 2>&1 | grep STARTED"
+                    }
+                }
+            }
         }
-      }
-    }
 
-    stage('Liveliness') {
-      steps {
-        script{
-          oss.withRun("-p 5701:5701 -e JAVA_OPTS=\"-Dhazelcast.rest.enabled=true\""){container ->
-            git changelog: false, poll: false, url: 'https://github.com/lazerion/hazelcast-docker-jenkins.git'
-            sh "chmod 777 wait.sh liveliness.sh"
-            sh "./wait.sh ${params.HOST}:5701 -t 10"
-            sh "./liveliness.sh ${params.HOST}"
-          }
+        stage('Liveliness') {
+            steps {
+                script {
+                    oss.withRun("-p 5701:5701 -e JAVA_OPTS=\"-Dhazelcast.rest.enabled=true\"") { container ->
+                        git changelog: false, poll: false, url: 'https://github.com/lazerion/hazelcast-docker-jenkins.git'
+                        sh "chmod 777 wait.sh liveliness.sh"
+                        sh "./wait.sh ${params.HOST}:5701 -t 10"
+                        sh "./liveliness.sh ${params.HOST}"
+                    }
+                }
+            }
         }
-      }
-    }
-  }
 
-  post {
-    always {
-      cleanWs deleteDirs: true
-      script{
-        sh "docker rmi ${oss.id}"
-      }
+        stage('Acceptance') {
+            steps {
+                script {
+                    oss.withRun("-p 5701:5701") { container ->
+                        sh "./wait.sh ${params.HOST}:5701 -t 10"
+                        sh "mvn test"
+                    }
+                }
+            }
+        }
     }
-    failure {
-      mail to: 'baris@hazelcast.com',
-      subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
-      body: "Something is wrong with ${env.BUILD_URL}"
+
+    post {
+        always {
+            cleanWs deleteDirs: true
+            script {
+                sh "docker rmi ${oss.id}"
+            }
+        }
+        failure {
+            mail to: 'baris@hazelcast.com',
+                    subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+                    body: "Something is wrong with ${env.BUILD_URL}"
+        }
     }
-  }
 }
